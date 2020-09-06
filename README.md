7405dd2c8eea294e9530d4b102576b07c9cba550
Dungduru <tjsxo21@naver.com>
2020-09-06 20:08:07 +0900

    helmet 추가

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # web_server_node
-node express 의 기본 골격
+node express 의 기본 골격  
+express를 사용해서 구조 구성  
+
+## express 프로젝트 생성
+
+```javascript
+express web_server_node
+```
+
+## ecosystem.config.js 파일 생성
+```javascript
+cd web_server_node
+pm2 ecosystem
+```
+
+## ecosystem.confi.js 파일 예시
+```javascript
+// 추후작성
+```
+
+## 기본적인 보안
+### Helmet 사용
+
+Helmet을 이용하면 HTTP 헤더를 적절히 설정하여 몇 가지 잘 알려진 웹 취약성으로부터 앱을 보호할 수 있다.  
+사실 Helmet은 보안 관련 HTTP 헤더를 설정하는 다음과 같은 더 작은 크기의 미들웨어 함수의 모음이다.
+
+```javascript
+app.use(helmet());
+```
+
+### 적어도 X-Powered-By 헤더는 사용하지 않도록 설정  
+Helmet의 사용을 원치 않는 경우에는 적어도 X-Powered-By 헤더는 사용하지 말 것
+공격자는 이 헤더(기본적으로 사용하도록 설정되어 있음)를 이용해 Express를 실행하는 앱을 발견한 후 특정한 대상에 대한 공격을 실행할 수 있다.
+
+```javascript
+app.disable('x-powered-by')
+```
--- app.js
+++ app.js
@@ -6,17 +6,13 @@ const logger = require('morgan');
 const cors = require('cors');
 const compression = require('compression');
 
-const session = require('express-session');
-const MemoryStore = require('memorystore')(session);
-const helmet = require('helmet');
-
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const session = require('express-session');             //세션
+const MemoryStore = require('memorystore')(session);    //세션 메모리 저장
+const helmet = require('helmet');                       //기본적인 보안모듈
 
 const app = express();
 
-const corsOptions = {
+const corsOptions = {     //cross origin 위배 방지 옵션?, cors는 클라이언트에서 조치해야 하는 걸로 알고 있는데
   origin: '*',
   optionsSuccessStatus: 200,
 }
@@ -28,23 +24,22 @@ app.set('view engine', 'ejs');                  //템플릿 엔진 ejs
 app.engine('html', require('ejs').renderFile);  //엔진 설정
 
 app.use(
-	session({
-		secret: '@#@$MYSIGN#@$#$',
-		resave: false,
-		saveUninitialized: true,
-		// cookie: {
-		//   maxAge: 1000 * 60 * 60 * 24 * 30 // 쿠키 유효기간 30일
-		// },
-		store: new MemoryStore({ checkPeriod: 86400000 }),
-	})
+  session({
+    secret: '@#@$MYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true,
+    // cookie: {
+    //   maxAge: 1000 * 60 * 60 * 24 * 30 // 쿠키 유효기간 30일
+    // },
+    store: new MemoryStore({ checkPeriod: 86400000 }),
+  })
 );
 
 // app.use(logger('dev'));
 app.use(logger('combined'));
 app.use(cors(corsOptions));
 app.use(compression());
-app.use(helmet());              //기본적인 보안 장치, Helmet은 보안 관련 HTTP 헤더를 설정하는 다음과 같은 더 작은 크기의 미들웨어 함수 9개의 모음입니다.
-app.use(helmet.noCache());
+app.use(helmet());              //기본적인 보안 장치
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -55,12 +50,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
